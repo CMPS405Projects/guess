009a43a3cb2e469385ac12646a1ab21eea6e9a9c
Aly Soliman <alysoliman05@gmail.com>
2024-04-28 17:31:58 +0300

    add `timestamp` function to class `Utils`

---
+++ src/utils/Utils.java
@@ -0,0 +1,10 @@
+package utils;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class Utils {
+    static String timestamp() {
+        return LocalDateTime.now().format(DateTimeFormatter.ISO_LOCAL_DATE_TIME);
+    }
+}
